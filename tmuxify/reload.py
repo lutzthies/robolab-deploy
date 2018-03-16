ebc2e518c56d3bd5afd9138d66f93f52063a5785
Kilian Koeltzsch <me@kilian.io>
2018-03-16 10:14:37 +0100

    run all the things

--- tmuxify/reload.py
+++ tmuxify/reload.py
@@ -1,4 +1,15 @@
 import sys, os, traceback
+
+def run():
+    try:
+        main.run()
+    except:
+        traceback.print_exc(file=sys.stdout)
+    if main.client:
+        main.client.loop_stop()
+    for motor in ev3.list_motors():
+        motor.stop()
+
 try:
     reloader.reload(main)
 except:
@@ -8,11 +19,4 @@ except:
 else:
     absolutely_unused_variable = os.system('clear')
     print('Reload successful.')
-    try:
-        main.run()
-    except:
-        traceback.print_exc(file=sys.stdout)
-    if main.client:
-        main.client.loop_stop()
-    for motor in ev3.list_motors():
-        motor.stop()
+    run()
--- tmuxify/run.py
+++ tmuxify/run.py
@@ -1,1 +1,13 @@
-main.run()
+import sys, os, traceback
+
+def run():
+    try:
+        main.run()
+    except:
+        traceback.print_exc(file=sys.stdout)
+    if main.client:
+        main.client.loop_stop()
+    for motor in ev3.list_motors():
+        motor.stop()
+
+run()
